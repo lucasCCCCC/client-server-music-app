8c77a67862cf577ae114d45d1c57523fc5e219fd
lucascdri <93845714+lucascdri@users.noreply.github.com>
2022-06-22 14:49:32 +0100

    Added credentials for connections

--- src/Credentials.java
+++ src/Credentials.java
@@ -1,2 +1,10 @@
 public class Credentials {
+
+    public static final String USERNAME = "postgres";
+    public static final String PASSWORD = "password";
+    public static final String URL = "jdbc:postgresql://localhost:5432/Music";
+
+    public static final String HOST = "127.0.0.1";
+    public static final int PORT = 80;
+
 }
